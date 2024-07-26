707b20e56c05c6b6d8d3161b4953ced6b160c4db
m.mukhammed <mukhammedmarat04@gmail.com>
2024-07-26 05:32:40 +0500

    feat: added navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,16 @@
 import HomePage from '../pages/homepage/HomePage';
 import Footer from '../widgets/footer/Footer';
 import Header from '../widgets/header/Header';
+import Navbar from '../widgets/navbar/Navbar';
+
 import { BrowserRouter } from 'react-router-dom';
 import './App.css'
+
 export default function App() {
   return (
     <BrowserRouter>
       <Header/>
+      <Navbar/>
       <HomePage/>
       <Footer/>
     </BrowserRouter>
--- src/widgets/navbar/Navbar.tsx
+++ src/widgets/navbar/Navbar.tsx
@@ -1,6 +1,37 @@
-export default function Navbar(){
-    return(
-        <>
-        </>
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import './navbar.css'
+
+const tabs = [
+    { tab: 'main', name: 'Главная' },
+    { tab: 'events', name: 'Событие' },
+    { tab: 'hobby', name: 'Хобби' },
+    { tab: 'homeRestaurant', name: 'Домашний ресторан' },
+    { tab: 'interiorOfTheDay', name: 'Интерьер дня' },
+    { tab: 'gallery', name: 'Галерея' },
+    { tab: 'collection', name: 'Коллекция' },
+    { tab: 'homeGarden', name: 'Домашний сад' }
+];
+
+export default function Navbar() {
+    const [activeTab, setActiveTab] = useState('main');
+
+    return (
+        <nav className="navbar">
+            <div className="container">
+                <div className="navbar__wrapper">
+                    {tabs.map(tab => (
+                        <Link
+                            key={tab.tab}
+                            to={`#${tab.tab}`}
+                            className={activeTab === tab.tab ? 'active' : ''}
+                            onClick={() => setActiveTab(tab.tab)}
+                        >
+                            {tab.name}
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </nav>
     );
 }
