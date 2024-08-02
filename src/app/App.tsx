16ebdea9702d233dc4ea866fee4fda28dd81e1d6
m.mukhammed <mukhammedmarat04@gmail.com>
2024-08-02 17:06:39 +0500

    added new pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 import HomePage from '../pages/homepage/HomePage';
 import RootLayout from './layout/RootLayout';
 import NotFoundPage from '../pages/notfoundpage/NotFoundPage';
+import RegisterPage from '../pages/registerpage/RegisterPage';
+import LoginPage from '../pages/loginpage/LoginPage';
 
 
 
@@ -10,7 +12,10 @@ export default function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<RootLayout />} >
+        {/* <Route path="/YurtaHome-frontend" element={<RootLayout />} > */}
             <Route index element={<HomePage />}/>
+            <Route path='/login' element={<LoginPage />}/>
+            <Route path='/register' element={<RegisterPage />}/>
             <Route path='*' element={<NotFoundPage/>}/>
         </Route>
     )
